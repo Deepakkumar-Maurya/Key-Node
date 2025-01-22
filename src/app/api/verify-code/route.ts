cab09211d4782927720f357fceab29a07e4c1900
Deepakkumar-Maurya <mauryadeepaktg2@gmail.com>
2025-01-22 21:33:18 +0530

    initial commit

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,96 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { verifySchema } from "@/schema/verifySchema";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    try {
+        const {username, code} = await request.json();
+
+        const verificationCode = {
+            code : code,
+        }
+
+        const result = verifySchema.safeParse(verificationCode);
+
+        if (!result.success) {
+            const codeErrors = result.error.format().code?._errors || [];
+            return Response.json(
+                {
+                    success: false,
+                    message: codeErrors.length > 0 ? codeErrors.join(", ") : "Invalid code provided",
+                },
+                {
+                    status: 400,
+                }
+            )
+        }
+
+        const user = await UserModel.findOne({
+            username,
+        });
+
+        if (!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found",
+                },
+                {
+                    status: 404,
+                }
+            )
+        }
+
+        if (user.isVerified) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User already verified",
+                },
+                {
+                    status: 400,
+                }
+            )
+        }
+
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = user.verifyCodeExpiry > new Date();
+
+        if (!isCodeValid || !isCodeNotExpired) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Invalid code provided",
+                },
+                {
+                    status: 400,
+                }
+            )
+        }
+
+        user.isVerified = true;
+        await user.save();
+        return Response.json(
+            {
+                success: true,
+                message: "Code verified successfully",
+            },
+            {
+                status: 200,
+            }
+        )
+    } catch (error) {
+        console.log("error in code verification", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error in code verification",
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+}
---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,30 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: email,
+            subject: "Mystery Message Verification Code",
+            react: VerificationEmail({ username, otp: verifyCode }),
+        });
+        console.log("email sent successfully");
+
+        return {
+            success: true,
+            message: "Verification email sent successfully",
+        };
+    } catch (error) {
+        console.log("error in sendVerificationEmail", error);
+        return {
+            success: false,
+            message: "Error sending verification email",
+        };
+    }
+}
